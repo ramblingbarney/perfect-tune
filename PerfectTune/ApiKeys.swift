905cf84e232ca366d1f1f1c187132dfeb23909cf
Conor O'Dwyer <c9dw5er@protonmail.com>
2020-02-20 11:34:30 +0000

    Load Api Keys From Local PList

---
+++ PerfectTune/ApiKeys.swift
@@ -0,0 +1,15 @@
+//
+//  ApiKeys.swift
+//  PerfectTune
+//
+//  Created by The App Experts on 20/02/2020.
+//  Copyright © 2020 Conor O'Dwyer. All rights reserved.
+//
+import Foundation
+
+func valueForAPIKey(named keyname:String) -> String {
+    let filePath = Bundle.main.path(forResource: "ApiKeys", ofType: "plist")
+    let plist = NSDictionary(contentsOfFile:filePath!)
+    let value = plist?.object(forKey: keyname) as! String
+    return value
+}
